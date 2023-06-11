a8a79dd293caef3d1928ac86bdf50f5894953949
Shidul Islam <sishahin093@gmail.com>
2023-06-11 11:32:37 +0600

    zod error handled

--- src/app/modules/users/middleware/globalErrorHandler.ts
+++ src/app/modules/users/middleware/globalErrorHandler.ts
@@ -1,28 +1,35 @@
 /* eslint-disable no-unused-expressions */
-import { ErrorRequestHandler } from 'express'
-import config from '../../../../config'
-import { IGenericErrorMessage } from '../../../../interfaces/error'
-import handleValidationError from '../../../../errors/handleVilidationError'
-import ApiError from '../../../../errors/ApiError'
-import { errorLogger } from '../../../../shared/logger'
+import { ErrorRequestHandler } from 'express';
+import config from '../../../../config';
+import { IGenericErrorMessage } from '../../../../interfaces/error';
+import handleValidationError from '../../../../errors/handleVilidationError';
+import ApiError from '../../../../errors/ApiError';
+import { errorLogger } from '../../../../shared/logger';
+import { ZodError } from 'zod';
+import handleZodError from '../../../../errors/handleZodError';
 
 const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
   config.env === 'development'
     ? console.log(' GlobalErrorHandler ', error)
-    : errorLogger.error('GlobalErrorHandler ', error)
+    : errorLogger.error('GlobalErrorHandler ', error);
 
-  let statusCode = 500
-  let message = 'Something went wrong!'
-  let errorMassages: IGenericErrorMessage[] = []
+  let statusCode = 500;
+  let message = 'Something went wrong!';
+  let errorMassages: IGenericErrorMessage[] = [];
 
   if (error?.name === 'ValidationError') {
-    const simplifiedError = handleValidationError(error)
-    statusCode = simplifiedError.statusCode
-    message = simplifiedError.message
-    errorMassages = simplifiedError.errorMassages
+    const simplifiedError = handleValidationError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMassages = simplifiedError.errorMassages;
+  } else if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMassages = simplifiedError.errorMassages;
   } else if (error instanceof ApiError) {
-    statusCode = error?.statusCode
-    message = error?.message
+    statusCode = error?.statusCode;
+    message = error?.message;
     errorMassages = error?.message
       ? [
           {
@@ -30,9 +37,9 @@ const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
             message: error?.message,
           },
         ]
-      : []
+      : [];
   } else if (error instanceof Error) {
-    message = error?.message
+    message = error?.message;
     errorMassages = error?.message
       ? [
           {
@@ -40,18 +47,18 @@ const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
             message: error?.message,
           },
         ]
-      : []
+      : [];
   }
   res.status(statusCode).json({
     success: false,
     message,
     errorMassages,
     stack: config.env !== 'production' ? error?.stack : undefined,
-  })
-  next()
-}
+  });
+  next();
+};
 
-export default globalErrorHandler
+export default globalErrorHandler;
 
 // path:
 // message:
--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -1,7 +1,13 @@
-import express from 'express'
-import { UserController } from './user.controller'
-const router = express.Router()
+import express from 'express';
+import { UserController } from './user.controller';
+import zodValidateRequest from './middleware/zodValidateRequest';
+import { UserZodValidation } from './user.zodValidation';
+const router = express.Router();
 
-router.post('/create-user', UserController.createUser)
+router.post(
+  '/create-user',
+  zodValidateRequest(UserZodValidation.createUserZodSchema),
+  UserController.createUser
+);
 
-export const UserRoutes = router
+export const UserRoutes = router;
