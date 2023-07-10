539dd1ccdb1885ba39430ddabe5fe761c1cdb2bc
Shidul Islam <sishahin093@gmail.com>
2023-07-10 10:31:28 +0600

    faculty update delete and get

--- src/app/modules/faculty/faculty.route.ts
+++ src/app/modules/faculty/faculty.route.ts
@@ -1,5 +1,20 @@
 import express from 'express';
+import { FacultyController } from './faculty.controller';
+import zodValidateRequest from '../users/middleware/zodValidateRequest';
+import { FacultyValidation } from './faculty.zodValidation';
 
 const router = express.Router();
+
+router.get('/:id', FacultyController.getSingleFaculty);
+
+router.get('/', FacultyController.getAllFaculties);
+
+router.patch(
+  '/:id',
+  zodValidateRequest(FacultyValidation.updateFacultyZodSchema),
+  FacultyController.updateFaculty
+);
+
+router.delete('/:id', FacultyController.deleteFaculty);
 
 export const FacultyRoutes = router;
---
+++ src/app/modules/faculty/faculty.service.ts
@@ -0,0 +1,138 @@
+import mongoose, { SortOrder } from 'mongoose';
+import { paginationHelper } from '../../../helpers/PaginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOption } from '../../../interfaces/pagination';
+import { IFaculty, IFacultyFilters } from './faculty.interface';
+import { Faculty } from './faculty.model';
+import { facultySearchableFields } from './faculty.constant';
+import ApiError from '../../../errors/ApiError';
+import httpStatus from 'http-status';
+import { User } from '../users/user.model';
+
+const getSingleFaculty = async (id: string): Promise<IFaculty | null> => {
+  const result = await Faculty.findOne({ _id: id })
+    .populate('academicDepartment')
+    .populate('academicFaculty');
+
+  return result;
+};
+
+const getAllFaculties = async (
+  filters: IFacultyFilters,
+  paginationOptions: IPaginationOption
+): Promise<IGenericResponse<IFaculty[]>> => {
+  // Extract searchTerm to implement search query
+  const { searchTerm, ...filtersData } = filters;
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelper.calculatePagination(paginationOptions);
+
+  const andConditions = [];
+
+  // Search needs $or for searching in specified fields
+  if (searchTerm) {
+    andConditions.push({
+      $or: facultySearchableFields.map(field => ({
+        [field]: {
+          $regex: searchTerm,
+          $options: 'i',
+        },
+      })),
+    });
+  }
+  // Filters needs $and to fullfill all the conditions
+  if (Object.keys(filtersData).length) {
+    andConditions.push({
+      $and: Object.entries(filtersData).map(([field, value]) => ({
+        [field]: value,
+      })),
+    });
+  }
+
+  // Dynamic  Sort needs  field to  do sorting
+  const sortConditions: { [key: string]: SortOrder } = {};
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder;
+  }
+  const whereConditions =
+    andConditions.length > 0 ? { $and: andConditions } : {};
+
+  const result = await Faculty.find(whereConditions)
+    .populate('academicDepartment')
+    .populate('academicFaculty')
+    .sort(sortConditions)
+    .skip(skip)
+    .limit(limit);
+
+  const total = await Faculty.countDocuments(whereConditions);
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
+const updateFaculty = async (
+  id: string,
+  payload: Partial<IFaculty>
+): Promise<IFaculty | null> => {
+  const isExist = await Faculty.findOne({ _id: id });
+
+  if (!isExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Faculty not found !');
+  }
+
+  const { name, ...FacultyData } = payload;
+  const updatedFacultyData: Partial<IFaculty> = { ...FacultyData };
+
+  if (name && Object.keys(name).length > 0) {
+    Object.keys(name).forEach(key => {
+      const nameKey = `name.${key}` as keyof Partial<IFaculty>;
+      (updatedFacultyData as any)[nameKey] = name[key as keyof typeof name];
+    });
+  }
+
+  const result = await Faculty.findOneAndUpdate({ id }, updatedFacultyData, {
+    new: true,
+  });
+  return result;
+};
+
+const deleteFaculty = async (id: string): Promise<IFaculty | null> => {
+  // check if the faculty is exist
+  const isExist = await Faculty.findOne({ id: id });
+
+  if (!isExist) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Faculty not found !');
+  }
+
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+    //delete faculty first
+    const faculty = await Faculty.findOneAndDelete({ id: id }, { session });
+    if (!faculty) {
+      throw new ApiError(404, 'Failed to delete student');
+    }
+    //delete user
+    await User.deleteOne({ id: id });
+    session.commitTransaction();
+    session.endSession();
+
+    return faculty;
+  } catch (error) {
+    session.abortTransaction();
+    throw error;
+  }
+};
+
+export const FacultyService = {
+  getSingleFaculty,
+  getAllFaculties,
+  updateFaculty,
+  deleteFaculty,
+};
